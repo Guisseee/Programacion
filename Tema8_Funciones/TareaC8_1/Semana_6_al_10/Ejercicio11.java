9f3a699086afcb15dfcbb35ec55ce790221e4001
guisseee <gjaulah651@g.educaand.es>
2023-02-16 14:40:20 +0100

    gjl

--- Tema8_Funciones/TareaC8_1/Semana_6_al_10/Ejercicio11.java
+++ Tema8_Funciones/TareaC8_1/Semana_6_al_10/Ejercicio11.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ejercicio11{
   public static void main(String[] args) {
   Scanner s= new Scanner(System.in);
-    System.out.print("Dime un número y te diré la tabla de mutiplicar");
+    System.out.print("Dime un número y te diré la tabla de mutiplicar: ");
     int num= s.nextInt();
     System.out.print("La tabla de multiplicar de "+num+" es: "+Varias.tablaMultiplicar(num));
   }
