5be04de1cd649501f91f14488d0971e26f0915cc
guisseee <gjaulah651@g.educaand.es>
2023-03-18 12:48:36 +0100

    GJL

---
+++ Tema9/TareaC9_2/Ejercicio1/Objetos/Gato.java
@@ -0,0 +1,32 @@
+package Tema9.TareaC9_2.Ejercicio1.Objetos;
+
+public class Gato {
+  // private static String comer;
+  private static String nombre;
+  private static String raza;
+  private static String color;
+
+  public String getNombre (){
+    return nombre;
+  }
+
+  public void setNombre (String n){
+    this.nombre= n;
+  }
+
+  public String getColor(){
+    return color;
+  }
+
+  public void setColor (String c){
+    this.color= c;
+  }
+
+  public String getRaza(){
+    return raza;
+  }
+
+  public void setRaza (String r){
+    this.raza= r;
+  }
+}
