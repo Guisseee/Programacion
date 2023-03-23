938fe98a26286bbcf320c02b45991893cc1a5643
Guisseee <123300253+Guisseee@users.noreply.github.com>
2023-03-23 20:32:00 +0100

    gjl

---
+++ Tema6 Numeros Aleatorios/TareaC6_3/Ejercicio17.java
@@ -0,0 +1,43 @@
+/*programa que pinte por pantalla una pecera con un pececito dentro.
+*Autor: Guillermo Jauregui Lahoz
+*/
+import java.util.Scanner;
+public class Ejercicio17 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca la altura de la pecera (como mínimo 4): ");
+    int alto= s.nextInt();
+
+    System.out.print("Ahora introduzca la anchura (como mínimo 4): ");
+    int ancho= s.nextInt();
+
+    int posicion = 0;
+
+    int posicionPez = (int)(Math.random()*(alto - 2)*(ancho - 2));
+
+    // Pinta la parte superior 
+    for(int i = 0; i < ancho; i++) {
+      System.out.print("*");
+    }
+    System.out.println();
+
+    // Pinta la parte central 
+    for(int i = 2; i < alto; i++) {
+      System.out.print("*"); // parte izquierda de la pecera
+      for(int j = 2; j < ancho; j++) {
+        if (posicion == posicionPez) {
+          System.out.print("&");
+        } else {
+          System.out.print(" ");
+        }
+        posicion++;
+      } // for j
+      System.out.println("*"); // parte derecha de la pecera
+    } // for i
+      
+    // Pinta la parte inferior 
+    for(int i = 0; i < ancho; i++) {
+      System.out.print("*");
+    }
+  }
+}
