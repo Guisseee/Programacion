9f9e323658005f41f120e93e6da5436301aa2d74
Guisseee <123300253+Guisseee@users.noreply.github.com>
2023-03-26 17:39:52 +0200

    gjl

--- Tema7 Arrays/Tarea C7.1/Jueves12-01-23/Ejercicio9.java
+++ Tema7 Arrays/Tarea C7.1/Jueves12-01-23/Ejercicio9.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Ejercici9 {
-
   public static void main(String[] args) {
-  
+  Scanner s= new Scanner(System.in);
+    
     int[] numero = new int[8];
     int i;
     
