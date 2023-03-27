ae7ac364c2ea8399e237de6d353bc8eb76c29208
guisseee <gjaulah651@g.educaand.es>
2023-03-27 16:30:22 +0200

    gjl

--- Tema8_Funciones/TareaC8_1/Jueves02_02_23/misFunciones/Varias.java
+++ Tema8_Funciones/TareaC8_1/Jueves02_02_23/misFunciones/Varias.java
@@ -134,4 +134,142 @@ public class Varias {
         }
         return (tabla);
     }
+    public static boolean esCapicua(long x) {
+        return x == voltea(x);
+    }
+    
+    /* Devuelve verdadero si el número que se pasa como parámetro es capicúa y
+    * falso en caso contrario.
+    * <p>
+    * Un número capicúa es el que se lee igual de izquierda a derecha que de
+    * derecha a izquierda.
+    *
+    * @param x número del que se quiere saber si es capicúa
+    * @return  verdadero si el número que se pasa como parámetro es capicúa y
+    *          falso en caso contrario
+    */
+    public static boolean esCapicua(int x) {
+        return esCapicua((long)x);
+    }
+
+    /* Devuelve verdadero si el número que se pasa como parámetro es primo y falso
+    * en caso contrario.
+    * <p>
+    * Un número primo es un número natural mayor que 1 y divisible únicamente
+    * entre el mismo y entre 1.
+    *
+    * @param x número del que se quiere saber si es primo
+    * @return  verdadero si el número que se pasa como parámetro es primo y falso
+    *          en caso contrario
+    */
+    public static Boolean esPrimo(long n) {
+        if (n < 2) {
+            return false;
+        } else {
+        for (long i = n / 2; i >= 2; i--) {
+            if (n % i == 0) {
+            return false;
+            }
+        }
+        }
+        return true;
+    }
+    
+    /* Devuelve verdadero si el número que se pasa como parámetro es primo y falso
+    * en caso contrario.
+    * <p>
+    * Un número primo es un número natural mayor que 1 y divisible únicamente
+    * entre el mismo y entre 1.
+    *
+    * @param x número del que se quiere saber si es primo
+    * @return  verdadero si el número que se pasa como parámetro es primo y falso
+    *          en caso contrario
+    */
+    public static Boolean esPrimo(int n) {
+        return esPrimo((long) n);
+    }
+
+    /* Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+    *
+    * @param x un número entero
+    * @return  el menor primo que es mayor al número que se pasa como parámetro
+    */
+    public static int siguientePrimo(int x) {
+        while (!esPrimo(++x)) {};
+        return x;
+    }
+    
+    /* Dada una base y un exponente, devuelve la potencia.
+    *
+    * @param base      base de la potencia
+    * @param exponente exponente de la potencia
+    * @return          número resultante de elevar la base a la potencia indicada
+    */
+    public static double potencia(int base, int exponente) {
+        if (exponente == 0) {
+        return 1;
+        }
+        if (exponente < 0) {
+        return 1/potencia(base, -exponente);
+        }
+        int n = 1;
+        for (int i = 0; i < Math.abs(exponente); i++) {
+          n = n * base;
+        }
+        return n;
+    }
+    
+    /* Cuenta el número de dígitos de un número entero.
+    *
+    * @param x número al que se le quieren contar los dígitos
+    * @return  número de dígitos que tiene el número que se pasa como parámetro
+    */
+    public static int digitos(long x) {
+        if (x < 0) {
+        x = -x;
+        }
+        if (x == 0) {
+            return 1;
+        } else {
+            int n = 0;
+        while (x > 0) {
+            x = x / 10; // se le quita un dígito a x
+            n++; // incrementa la cuenta de dígitos
+        }
+        return n;
+        }
+    }
+    
+    /* Cuenta el número de dígitos de un número entero.
+    * @param x número al que se le quieren contar los dígitos
+    * @return  número de dígitos que tiene el número que se pasa como parámetro
+    */
+    public static int digitos(int x) {
+        return digitos((long)x);
+    }
+    
+    /* Le da la vuelta a un número.
+    * @param x número al que se le quiere dar la vuelta
+    * @return  número volteado (al revés)
+    */
+    public static long voltea(long x) {
+        if (x < 0) {
+        return -voltea(-x);
+        }
+        long volteado = 0;
+        while (x > 0) {
+            volteado = (volteado * 10) + (x % 10);
+            x = x / 10;
+        }
+        return volteado;
+    }
+    
+    /* Le da la vuelta a un número.
+    *
+    * @param x número al que se le quiere dar la vuelta
+    * @return  número volteado (al revés)
+    */
+    public static int voltea(int x) {
+        return (int)voltea((long)x);
+    }
 }
---
+++ Tema8_Funciones/TareaC8_1/Viernes27_01_23/package Tema8_Funciones.TareaC8_1.java
@@ -0,0 +1,60 @@
+package Tema8_Funciones.TareaC8_1.Viernes27_01_23;
+import Tema8_Funciones.TareaC8_1.Jueves02_02_23.misFunciones.Varias;
+/**
+ * 8. Funciones
+ *
+ * Ejercicios 1 a 14
+ *
+ * @author Luis José Sánchez
+ */
+public class S08Ejercicio01a14 {
+
+  public static void main(String[] args) {
+
+    // esCapicua ///////////////////////////////////////////////////
+
+    if (Varias.esCapicua(29)) {
+      System.out.println("El 29 es capicúa");
+    }
+
+    if (Varias.esCapicua(464)) {
+      System.out.println("El 464 es capicúa");
+    }
+
+    // esPrimo /////////////////////////////////////////////////////
+
+    if (Varias.esPrimo(29)) {
+      System.out.println("El 29 es primo");
+    }
+
+    if (Varias.esPrimo(80)) {
+      System.out.println("El 80 es primo");
+    }
+
+    // siguientePrimo //////////////////////////////////////////////
+
+    System.out.println("El siguiente primo mayor a 23 es "
+        + Varias.siguientePrimo(23));
+    System.out.println("El siguiente primo mayor a 100 es "
+        + Varias.siguientePrimo(100));
+
+    // potencia ////////////////////////////////////////////////////
+
+    System.out.println("2^10 = " + Varias.potencia(2, 10));
+    System.out.println("5^(-3) = " + Varias.potencia(5, -3));
+    System.out.println("10^6 = " + Varias.potencia(10, 6));
+
+    // digitos /////////////////////////////////////////////////////
+
+    System.out.println("El número 0 tiene " + Varias.digitos(0) + " dígito/s.");
+    System.out.println("El número 7 tiene " + Varias.digitos(7) + " dígito/s.");
+    System.out.println("El número 674893123 tiene " + Varias.digitos(674893123)
+        + " dígito/s.");
+
+    // voltea //////////////////////////////////////////////////////
+
+    System.out.println("El 5 volteado es " + Varias.voltea(5));
+    System.out.println("El 398004321 volteado es " + Varias.voltea(398004321));
+    System.out.println("El -75839 volteado es " + Varias.voltea(-75839));
+  }
+}
